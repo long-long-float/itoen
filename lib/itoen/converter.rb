ccfbbc46aac226a8e78acd5e2cc38c0907e1d1b8
long-long-float <niinikazuki@yahoo.co.jp>
2014-03-25 19:57:57 +0900

    convert 11 ~ 20

--- lib/itoen/converter.rb
+++ lib/itoen/converter.rb
@@ -2,6 +2,7 @@ module ITOEN
   module_function
   
   def itoen(num)
-    %w(zero one two three four five six seven eight nine ten)[num]
+    %w(zero one two three four five six seven eight nine ten
+      eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty)[num]
   end
 end
--- spec/itoen_spec.rb
+++ spec/itoen_spec.rb
@@ -5,9 +5,15 @@ describe ITOEN do
     ITOEN::VERSION.should_not be_nil
   end
 
-  it 'returns numbers which less than 10' do
+  it 'returns English numbers (0..10)' do
     [*0..10].map{|num|
       ITOEN.itoen(num)
     }.should eq %w(zero one two three four five six seven eight nine ten)
+  end
+
+  it 'returns English numbers (11..20)' do
+    [*11..20].map{|num|
+      ITOEN.itoen(num)
+    }.should eq %w(eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty)
   end
 end
